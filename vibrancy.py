0d37c0288a030c58c03e0c3603d4eb2b61460472
Andre Barbe <31875393+andre-barbe@users.noreply.github.com>
2022-02-01 20:20:44 -0500

    Added all the scenarios and changes that Daniel asked for in Jan 25 meeting and email update

--- vibrancy.py
+++ vibrancy.py
@@ -3,10 +3,12 @@
 import pandas as pd
 import numpy as np
 
+pass
+
 # ===============Import raw data from Excel sheets of Vibrancy website files===============
 # How to read from excel https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
 # Need to use engine option as described here: https://stackoverflow.com/questions/48066517/python-pandas-pd-read-excel-giving-importerror-install-xlrd-0-9-0-for-excel
-version_year = 2021 #flag to import either the 2021 or 2022 version of the data
+version_year = 2022 #flag to import either the 2021 or 2022 version of the data
 if version_year == 2021:
     data_raw_research = pd.read_excel("data/raw/MAG - 2021 AI Index Report (Main).xlsx", sheet_name="By CountryRegion", engine='openpyxl')
     data_raw_investment_amount = pd.read_excel("data/raw/NetBase Quid - 2021 AI Index Report.xlsx", sheet_name="Investment Amount", engine='openpyxl')
@@ -84,6 +86,7 @@ if version_year == 2022:
     data_research = data_research_wide.reset_index()  # the pivot changed country name to the df index. This changes Country name from the df's index back to a variable
     data_research["Country Name"] = data_research["Country Name"].str.title()  # Capitalize all the same to make merging later easier
 
+pass
 
 # =============== Clean 2021 Data =====================
 # =============== Filter research data ===============
@@ -144,13 +147,14 @@ data_main = data_main.merge(data_research, on="Country Name",
 data_main = data_main.merge(data_ranking_2020, on="Country Name",
                             how="outer")  # Outer merge keeps data that is in either
 
+pass
+
 # =============== Clean the merged Data ===============
 data_main = data_main.sort_values("Country Name")
 
 data_main = data_main.loc[data_main["Country Name"] != "-"]
 data_main = data_main.loc[data_main["Country Name"] != "None Listed"]
 data_main = data_main.loc[~pd.isna(data_main["Country Name"])]  # Drop if country name is missing
-# data_main = data_main.dropna() ##Drop entries that contain any missing values
 
 # Create normalized version of variables
 list_variables_to_normalize = list(data_main.drop(["Country Name","2020 Vibrancy Rank (only absolute metrics)"],axis=1)) # Create list of all variables except "Country Name" and the 2020 ranking. See https://stackoverflow.com/questions/29763620/how-to-select-all-columns-except-one-column-in-pandas
@@ -160,8 +164,6 @@ for variable_name in list_variables_to_normalize:
 # Create new variable that lists the number of NaNs in normalized variables
 data_main["Number NaN"]=data_main[list_variables_to_normalize].isnull().sum(axis=1) # Creates new column with number of NANs in each row. See https://stackoverflow.com/questions/30059260/python-pandas-counting-the-number-of-missing-nan-in-each-row
 
-# TODO: UPDATE weighting schemes and add scenarios as requested by Daniel
-
 # Create my new indexes using each of the 3 weighting methods
 list_normalized_variables = [''.join(variable_name) + " Normalized" for variable_name in list_variables_to_normalize] # Create list of normalized variable names
 if version_year==2021:
@@ -169,22 +171,30 @@ if version_year==2021:
     data_main["Index Equal Pillar Weights"] = data_main[list_normalized_variables].mul([8/2,8/2,1,1,1,1,1,1,1,1]).sum(1) #There are 8 doc types, so research has a total weight of 8
     data_main["Index Andre Prefers"] = data_main[list_normalized_variables].mul([0,8,1,1,1,1,1,1,1,1]).sum(1) #There are 8 doc types, so research has a total weight of 8
 if version_year==2022:
-    data_main["Index Equal Metric Weights"] = data_main[list_normalized_variables].mul([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).sum(1) # Multiply by vector weights and then sums, as described here: https://stackoverflow.com/questions/47026517/multiply-rows-in-dataframe-then-sum-them-together-python
-    data_main["Index Equal Pillar Weights"] = data_main[list_normalized_variables].mul([14/2,14/2,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).sum(1) #There are 7 doc types and ctiation vs publ, so research has a total weight of 14
-    data_main["Index Andre Prefers"] = data_main[list_normalized_variables].mul([0,14,1,1,1,1,1,1,1,0,0,0,0,0,0,0]).sum(1) #There are 7 doc types and ctiation vs publ, so research has a total weight of 14
+    data_main["Score 1 - No Citations, Equal Metrics"] = data_main[list_normalized_variables].mul([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).sum(1) # Multiply by vector weights and then sums, as described here: https://stackoverflow.com/questions/47026517/multiply-rows-in-dataframe-then-sum-them-together-python
+    data_main["Score 2 - No Citations, Equal Pillars"] = data_main[list_normalized_variables].mul([14/2,14/2,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).sum(1) #There are 7 doc types and ctiation vs publ, so research has a total weight of 14
+    data_main["Score 3 - With Citations, Equal Metrics"] = data_main[list_normalized_variables].mul([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).sum(1) # Multiply by vector weights and then sums, as described here: https://stackoverflow.com/questions/47026517/multiply-rows-in-dataframe-then-sum-them-together-python
+    data_main["Score 4 - With Citations, Equal Pillars"] = data_main[list_normalized_variables].mul([14/2,14/2,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).sum(1) #There are 7 doc types and ctiation vs publ, so research has a total weight of 14
+
 
 # Create rankings for each country
-data_main[['Rank Equal Metric Weights', 'Rank Equal Pillar Weights','Rank Andre Prefers']] = data_main[['Index Equal Metric Weights','Index Equal Pillar Weights','Index Andre Prefers']].rank(ascending= False).astype(int) # Create rankings for each of the new weighting methods
-data_main = data_main.sort_values("Rank Equal Metric Weights") # Sort countries by ranking to make display easier
+if version_year==2021:
+    data_main[['Rank Equal Metric Weights', 'Rank Equal Pillar Weights','Rank Andre Prefers']] = data_main[['Index Equal Metric Weights','Index Equal Pillar Weights','Index Andre Prefers']].rank(ascending= False).astype(int) # Create rankings for each of the new weighting methods
+    data_main = data_main.sort_values("Rank Equal Metric Weights") # Sort countries by ranking to make display easier
+if version_year==2022:
+    list_score_variables = data_main.columns[pd.Series(list(data_main)).str.contains("Score", na=False).tolist()] #Use NA=False so that for tuple variable names, it will be false. Lots of conversion between series and list. Very annoying. See https://pandas.pydata.org/docs/reference/api/pandas.Series.tolist.html and https://pandas.pydata.org/docs/reference/api/pandas.Series.str.match.html
+    for variable_name in list_score_variables:
+        data_main['Rank '+ variable_name] = data_main[variable_name].rank(ascending= False).astype(int) # Create rankings for each of the new weighting methods
+    data_main = data_main.sort_values("Rank "+ list_score_variables[0]) # Sort countries by ranking to make display easier
 
 # Calculate the max difference in Country Rank between the Various weighting schemes
-data_main['Rank Difference Max'] = np.maximum(
-    np.maximum( #Need to use np maximum command for pairwise vector max, see https://stackoverflow.com/questions/51813621/pandas-series-pairwise-maximum
-        abs(data_main['Rank Andre Prefers']-data_main['Rank Equal Pillar Weights']),
-        abs(data_main['Rank Andre Prefers']-data_main['Rank Equal Metric Weights'])
-    ),
-    abs(data_main['Rank Equal Metric Weights']-data_main['Rank Equal Pillar Weights'])
-)
+if version_year==2021:
+    data_main['Max Rank']= np.maximum(data_main['Rank Andre Prefers'],data_main['Rank Equal Pillar Weights'],data_main['Rank Equal Metric Weights']) #Need to use np maximum command for pairwise vector max, see https://stackoverflow.com/questions/51813621/pandas-series-pairwise-maximum
+    data_main['Min Rank']= np.minimum(data_main['Rank Andre Prefers'],data_main['Rank Equal Pillar Weights'],data_main['Rank Equal Metric Weights'])
+    data_main['Rank Difference Max'] = data_main['Max Rank'] - data_main['Min Rank']
+if version_year==2022:
+    list_rank_variables = data_main.columns[pd.Series(list(data_main)).str.contains("Rank", na=False).tolist()] #Use NA=False so that for tuple variable names, it will be false. Lots of conversion between series and list. Very annoying. See https://pandas.pydata.org/docs/reference/api/pandas.Series.tolist.html and https://pandas.pydata.org/docs/reference/api/pandas.Series.str.match.html
+    data_main['Rank Difference Max'] = data_main[list_rank_variables].max(axis=1) - data_main[list_rank_variables].min(axis=1)
 
 # Export results to CSV
 data_main.to_csv("reports/vibrancy_data_main_" + str(version_year) + ".csv")
